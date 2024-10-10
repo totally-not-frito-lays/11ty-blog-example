0054839f1cc5de27d4ce56962f8f936a3457c6a8
Higgins-Codes <30853424+totally-not-frito-lays@users.noreply.github.com>
2024-10-10 08:57:04 -0700

    got playwright to run locally

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,17 +2,17 @@
 const { test, expect } = require('@playwright/test');
 const port = process.env.PORT || 8080;
 
-test('asserts that codespaces open port is working', async ({ page }) => {
-  await page.goto(`https://friendly-xylophone-vp49j6g69jwcr9p-${port}.app.github.dev/`);
-  await expect(page).toHaveTitle(/Codespaces Access Port/);
-});
-
-// test('has title', async ({ page }) => {
-  //   await page.goto(`https://friendly-xylophone-vp49j6g69jwcr9p-${port}.app.github.dev/`);
-
-//   // Expect a title "to contain" a substring.
-//   await expect(page).toHaveTitle(/Eleventy Base Blog v8/);
+// test('asserts that codespaces open port is working', async ({ page }) => {
+//   await page.goto(`https://friendly-xylophone-vp49j6g69jwcr9p-${port}.app.github.dev/`);
+//   await expect(page).toHaveTitle(/Codespaces Access Port/);
 // });
+
+test('has title', async ({ page }) => {
+  await page.goto(`http://localhost:${port}`);
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Eleventy Base Blog v8/);
+});
 
 // test('get started link', async ({ page }) => {
 //   await page.goto(`https://friendly-xylophone-vp49j6g69jwcr9p-${port}.app.github.dev/`);
